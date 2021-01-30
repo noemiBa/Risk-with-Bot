ee8f5cf615e1416bbc416be4b03521eedd3f0067
noemiBa <77796348+noemiBa@users.noreply.github.com>
2021-01-30 19:12:45 +0000

    Added comments

--- Country.java
+++ Country.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import javax.swing.*;
 
 public class Country {
+	//declaration of instance variables
 	private String name; 
 	private int coord_x;
 	private int coord_y;
@@ -13,6 +14,11 @@ public class Country {
 	public static final String[] CONTINENT_NAMES = {"N America","Europe","Asia","Australia","S America","Africa"};  
 	public static final int[] CONTINENT_VALUES = {5,5,7,2,2,3};
 	
+	/** Constructor: constructs the object Country. Here the continent index is set to -1 to start, and the arrayList of adjacent countries initialised.
+	* @param name: the name of the country
+	* @param coord_x: the x coordinate of the country on the Map
+	* @param coord_y: the y coordinate of the country on the Map
+	*/
 	public Country(String name, int coord_x, int coord_y) {
 		this.name = name; 
 		this.coord_x = coord_x; 
@@ -20,7 +26,16 @@ public class Country {
 		this.continent = -1;
 		this.adjCountries = new ArrayList<Integer>(); 
 	}
+	
+	//Accessor methods 
+	public String getName() {
+		return name;
+	}
 
+	public int getCoord_x() {
+		return coord_x;
+	}
+	
 	public int getContinent() {
 		return continent;
 	}
@@ -28,19 +43,34 @@ public class Country {
 	public ArrayList<Integer> getAdjCountries() {
 		return adjCountries;
 	}
-
-	public void setAdjCountries(ArrayList<Integer> adjCountries) {
-		this.adjCountries = adjCountries;
-	}
 	
 	public int getAdjCountriesLength() {
 		return adjCountries.size();
+	}
+	
+	public int getCoord_y() {
+		return coord_y;
+	}
+	
+	//mutator methods
+	public void setCoord_x(int coord_x) {
+		this.coord_x = coord_x;
+	}
+
+	public void setCoord_y(int coord_y) {
+		this.coord_y = coord_y;
+	}
+	public void setAdjCountries(ArrayList<Integer> adjCountries) {
+		this.adjCountries = adjCountries;
 	}
 
 	public void setContinent(int contintent) {
 		this.continent = contintent;
 	}
 	
+	/**
+	* This method returns the String name of the Continent the Country belongs to.
+	*/
 	public String getContinentName() {
 		switch (getContinent()) {
 		case 0: return CONTINENT_NAMES[0];
@@ -52,29 +82,9 @@ public class Country {
 		default: return "Invalid";
 		}
 	}
-
-	public String getName() {
-		return name;
-	}
-
-	public int getCoord_x() {
-		return coord_x;
-	}
-
-	public void setCoord_x(int coord_x) {
-		this.coord_x = coord_x;
-	}
-
-	public int getCoord_y() {
-		return coord_y;
-	}
-
-	public void setCoord_y(int coord_y) {
-		this.coord_y = coord_y;
-	}
 	
 	/*
-	 * For testing purposes
+	 * For testing purposes. Returns a String representation of the Country class.
 	 */
 	@Override
 	public String toString() {
