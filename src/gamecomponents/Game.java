ec3d9ecb3e6eda1004d2a794d7a93b5fbd8f163f
BecaBuarque <77796842+BecaBuarque@users.noreply.github.com>
2021-02-04 21:24:56 +0000

    Players Instantiated
    Fix assignCountries(), it is not returning the name of the countries

--- src/gamecomponents/Game.java
+++ src/gamecomponents/Game.java
@@ -5,28 +5,77 @@ import player.PassivePlayer;
 import player.Player;
 import ui.Window;
 
-/** Game class created to run different the main class methods in each of the packages
+import java.util.ArrayList;
+
+/**
+ * Game class created to run different the main class methods in each of the packages
  * Currently used to test creating an Array of player objects and .draw() method
- * */
+ */
 
-public class Game
-{
-    public Game() {};
-    public static void main()
-    {
+public class Game {
+    private static Player[] players;
+    public static String namePlayer1;
+    public static String namePlayer2;
+
+    public Game() {
+
+    }
+
+    ;
+
+    public static void main() {
+        Game game = new Game();
+        Window window = new Window();
+
+
+        Player[] players = game.getPlayers();
+
+        for (Player p : players) {
+            System.out.println(p.getName() + " in Game");
+        }
+    }
+
+
+    public String getNamePlayer1() {
+        return namePlayer1;
+    }
+
+    public void setNamePlayer1(String namePlayer1) {
+        this.namePlayer1 = namePlayer1;
+    }
+
+    public String getNamePlayer2() {
+        return namePlayer2;
+    }
+
+    public void setNamePlayer2(String namePlayer2) {
+        this.namePlayer2 = namePlayer2;
+    }
+
+    public Player[] getPlayers() {
+        return startPlayers();
+    }
+
+
+    public Player[] startPlayers() {
         Player[] players = {
-                new ActivePlayer("test1", Player.color.GREEN),
-                new ActivePlayer("test2", Player.color.GREEN),
-                new PassivePlayer("test3", Player.color.GREEN),
-                new PassivePlayer("test4", Player.color.GREEN),
-                new PassivePlayer("test5", Player.color.GREEN),
-                new PassivePlayer("test6", Player.color.GREEN)
-
-        };
-
+                new ActivePlayer(getNamePlayer1(), ActivePlayer.color.BLUE),
+                new ActivePlayer(getNamePlayer2(), ActivePlayer.color.GREEN),
+                new PassivePlayer("Benny", PassivePlayer.color.BROWN),
+                new PassivePlayer("Harry", PassivePlayer.color.GREY),
+                new PassivePlayer("Jolene", PassivePlayer.color.ORANGE),
+                new PassivePlayer("Borgov", PassivePlayer.color.YELLOW)};
+        for (Player p : players) {
+            System.out.println(p.getName() + " in Window");
+        }
         Deck deck = new Deck();
         deck.shuffle();
         Player.assignCountries(players, deck);
-        Window.main();
+
+        for (Player p : players) {
+            System.out.println(p.getName() + " has the countries: " + p.getCards());
+        }
+        System.out.println("players instantiated");
+        return players;
     }
 }
