2cbe98032901b45bd0c5aa63caf418550b7598c1
BecaBuarque <77796842+BecaBuarque@users.noreply.github.com>
2021-02-04 21:21:52 +0000

    Update Map.java

--- src/ui/Map.java
+++ src/ui/Map.java
@@ -7,16 +7,16 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.util.ArrayList;
 import javax.swing.JPanel;
-import java.awt.Font; 
+import java.awt.Font;
 
 public class Map extends JPanel {
 
     /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
-	/**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
      * Instance variables
      */
     protected ArrayList<Country> countries;
@@ -24,24 +24,23 @@ public class Map extends JPanel {
             "Ontario","Quebec","NW Territory","Alberta","Greenland","E United States","W United States","Central America","Alaska",
             "Great Britain","W Europe","S Europe","Ukraine","N Europe","Iceland","Scandinavia",
             "Afghanistan","India","Middle East","Japan","Ural","Yakutsk","Kamchatka","Siam","Irkutsk","Siberia","Mongolia","China",
-            "E Australia","New Guinea","W Australia","Indonesia",
-            "Venezuela","Peru","Brazil","Argentina",
+            "E Australia","New Guinea","W Australia","Indonesia", "Venezuela","Peru","Brazil","Argentina",
             "Congo","N Africa","S Africa","Egypt","E Africa","Madagascar"};
     public static final int[][] ADJACENT = {{4,1,5,6,3,2},{4,5,0},{4,0,3,8},{2,0,6,8},{14,1,0,2},{0,1,7,6},{3,0,5,7},{6,5,32},{2,3,22},{14,15,13,10},
             {9,13,11,37}, {13,12,18,39,10},{20,16,18,11,13,15},{15,12,11,10,9},{15,9,4},{12,13,14},{20,27,17,18,12}, {16,27,23,18},
             {12,16,17,40,39,11},{26,22},{25,27,16,12},{22,24,25},{8,19,26,24,21},{27,31,17},{21,22,26,25},{21,24,26,27,20},{24,22,19,27,25},{26,23,17,16,20,25},
             {29,30},{28,30,31},{29,28,31},{23,29,30},{7,34,33},{32,34,35},{32,37,35,33},{33,34},{37,40,38},{10,11,39,40,36,34},{36,40,41},{11,18,40,37},
             {39,18,41,38,36,37},{38,40}};
-    private static final int[][] COUNTRY_COORD = {{191,150},{255,161},{146,86},{123,144},{314,61},{205,235},{135,219},{140,299},{45,89},{370,199},{398,280},
-            {465,270},{547,180},{460,200},{393,127},{463,122},{628,227},{679,332},{572,338},{861,213},{645,152},{763,70},{827,94},{751,360},{750,140},{695,108},{760,216},{735,277},{889,537},
-            {850,429},{813,526},{771,454},{213,352},{221,426},{289,415},{233,523},{496,462},{440,393},{510,532},{499,354},{547,432},{586,545}};
+    private static final int[][] COUNTRY_COORD = {{213,170},{265,185},{146,122},{146,170},{310,100},{210,260},{153,225},{155,288},{75,130},{352,215},{365,305},
+            {427,294},{505,200},{415,247},{373,160},{443,143},{567,252},{610,340},{514,330},{770,235},{584,190},{705,110},{770,120},{678,360},{686,180},{620,130},{687,230},{665,290},
+            {785,505},{755,418},{700,513},{675,440},{208,350},{225,427},{283,405},{235,485},{458,450},{400,380},{463,520},{459,357},{505,420},{528,513}};
     public static final int[] CONTINENT_IDS = {0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,5,5};
-    private static final int MAP_WIDTH = 1000;
-    private static final int MAP_HEIGHT = 600;
+    private static final int MAP_WIDTH = 1100;
+    private static final int MAP_HEIGHT = 700;
     private static final int DIAMETER = 30;
     private static final int RADIUS = DIAMETER/2;
     private static final int NUM_COUNTRIES = COUNTRY_COORD.length;
-    private static final int SPACING = 5; 
+    private static final int SPACING = 5;
     /**
      * Constructor for the Map class. The Constructor takes no argument and simply initialises the array list of Countries.
      */
@@ -64,15 +63,15 @@ public class Map extends JPanel {
                 adjCountries.add(ADJACENT[i][k]);
             }
             countries.get(i).setAdjCountries(adjCountries);
-            System.out.println(countries.get(i).toString()); //for testing purposes
+         //   System.out.println(countries.get(i).toString()); //for testing purposes
         }
     }
-    
-    public static String[] getCountryNames() {
-		return COUNTRY_NAMES;
-	}
 
-	/**
+    public static String[] getCountryNames() {
+        return COUNTRY_NAMES;
+    }
+
+    /**
      * Overrides the getPreferredSize() method of JPanel. It sets the Panel dimensions to MAP_HEIGHT and MAP_WIDTH.
      */
     @Override
@@ -86,28 +85,28 @@ public class Map extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-       
+
         for (Country c: countries) { //draw the sea lines first as they will be in the background.
-        	for (int i = 0; i<c.getAdjCountriesLength(); i++) {
-        		 //do NOT draw the sea line between Alaska and Kamchatka in the middle of the screen. 
-        		 if ((c.getName() != "Alaska" || countries.get(c.getAdjCountries().get(i)).getName() != "Kamchatka") && (c.getName() != "Kamchatka" || countries.get(c.getAdjCountries().get(i)).getName() != "Alaska")) {
-        			 g.setColor(Color.LIGHT_GRAY);
-        			 g.drawLine(c.getCoord_x(), c.getCoord_y(), countries.get(c.getAdjCountries().get(i)).getCoord_x(), countries.get(c.getAdjCountries().get(i)).getCoord_y());
-        		 }
-        	}
+            for (int i = 0; i<c.getAdjCountriesLength(); i++) {
+                //do NOT draw the sea line between Alaska and Kamchatka in the middle of the screen.
+                if ((c.getName() != "Alaska" || countries.get(c.getAdjCountries().get(i)).getName() != "Kamchatka") && (c.getName() != "Kamchatka" || countries.get(c.getAdjCountries().get(i)).getName() != "Alaska")) {
+                    g.setColor(Color.LIGHT_GRAY);
+                    g.drawLine(c.getCoord_x(), c.getCoord_y(), countries.get(c.getAdjCountries().get(i)).getCoord_x(), countries.get(c.getAdjCountries().get(i)).getCoord_y());
+                }
+            }
         }
-        
-          //draw the sea lines between Alaska and Kamchatka
-           g.drawLine(countries.get(Country.getIndex("Alaska")).getCoord_x(), countries.get(Country.getIndex("Alaska")).getCoord_y(), 0, countries.get(Country.getIndex("Alaska")).getCoord_y());	
-           g.drawLine(countries.get(Country.getIndex("Kamchatka")).getCoord_x(), countries.get(Country.getIndex("Kamchatka")).getCoord_y(), MAP_WIDTH, countries.get(Country.getIndex("Kamchatka")).getCoord_y());
-      
-           for (Country c : countries) {
-        	//draw the country names
-        	g.setColor(Color.black); 
-        	g.setFont(new Font("TimesRoman", Font.PLAIN, 16));
-        	g.drawString(c.getName(), c.getCoord_x()-DIAMETER, c.getCoord_y()+DIAMETER); 
-        	
-        	//draw the graph nodes to indicate the countries changing color according to the continent they belong to.
+
+        //draw the sea lines between Alaska and Kamchatka
+        g.drawLine(countries.get(Country.getIndex("Alaska")).getCoord_x(), countries.get(Country.getIndex("Alaska")).getCoord_y(), 0, countries.get(Country.getIndex("Alaska")).getCoord_y());
+        g.drawLine(countries.get(Country.getIndex("Kamchatka")).getCoord_x(), countries.get(Country.getIndex("Kamchatka")).getCoord_y(), MAP_WIDTH, countries.get(Country.getIndex("Kamchatka")).getCoord_y());
+
+        for (Country c : countries) {
+            //draw the country names
+            g.setColor(Color.black);
+            g.setFont(new Font("TimesRoman", Font.PLAIN, 16));
+            g.drawString(c.getName(), c.getCoord_x()-DIAMETER, c.getCoord_y()+DIAMETER);
+
+            //draw the graph nodes to indicate the countries changing color according to the continent they belong to.
             if (c.getContinent() == 0) {
                 g.setColor(Color.green);
                 g.fillOval(c.getCoord_x()-RADIUS, c.getCoord_y()-RADIUS, DIAMETER, DIAMETER);
@@ -131,13 +130,13 @@ public class Map extends JPanel {
             else if (c.getContinent() == 5) {
                 g.setColor(Color.blue);
                 g.fillOval(c.getCoord_x()-RADIUS, c.getCoord_y()-RADIUS, DIAMETER, DIAMETER);
-            }   
+            }
         }
-        
+
         //Draw the number of military units on the country node.
         for (Country c: countries) {
-        	g.setColor(Color.black);
-        	g.drawString(String.valueOf(c.getNumberOfUnits()), c.getCoord_x()-SPACING, c.getCoord_y()+SPACING);
+            g.setColor(Color.black);
+            g.drawString(String.valueOf(c.getNumberOfUnits()), c.getCoord_x()-SPACING, c.getCoord_y()+SPACING);
         }
     }
 }
