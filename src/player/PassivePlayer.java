2640620d662f7f6158bb8664e1c1aaad244debf2
BecaBuarque <77796842+BecaBuarque@users.noreply.github.com>
2021-02-04 21:29:01 +0000

    Update PassivePlayer.java

--- src/player/PassivePlayer.java
+++ src/player/PassivePlayer.java
@@ -5,12 +5,47 @@ import gamecomponents.Deck;
 
 import java.util.ArrayList;
 
-/** extends player class
+/** implements Player interface
  * */
-public class PassivePlayer extends Player
+public class PassivePlayer implements Player
 {
+    public enum color {BROWN, GREEN, GREY, BLUE, YELLOW, ORANGE};
+
+    private String name;
+    private color playerColor;
+    private ArrayList<Card> cards;
+
     public PassivePlayer(String name, color playerColor)
     {
-        super(name, playerColor);
+        this.name = name;
+        this.playerColor = playerColor;
+        cards = new ArrayList <Card>();
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public color getPlayerColor()
+    {
+        return playerColor;
+    }
+
+    public ArrayList<Card> getCards()
+    {
+        return cards;
+    }
+
+    /** Removes the number of cards specified from the top of the deck ArrayList and
+     * adds to Player cards ArrayList
+     * */
+    public void draw(int numberOfCards, Deck deck)
+    {
+        for(int i = 1; i <= numberOfCards; i++)
+        {
+            cards.add(deck.getCards().get(0));
+            deck.getCards().remove(0);
+        }
     }
 }
