60694fd7e11f9272db809660f8367c7e536f706d
BecaBuarque <77796842+BecaBuarque@users.noreply.github.com>
2021-02-06 15:48:31 +0000

    Update Window.java

--- src/ui/Window.java
+++ src/ui/Window.java
@@ -1,15 +1,13 @@
 package ui;
 
 import gamecomponents.Game;
+import player.Player;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import player.*; 
+
 /*
  * @Author: BotHarmon (Jess Dempsey, Rebeca Buarque, Noemi Banal)
  */
@@ -22,7 +20,6 @@ public class Window {
     private static JPanel readText;
     private static JLabel labelDisplayText;
     private static int instruction;
-    JFrame frame;
 
     public Window(Game game) {
         instruction = 1;
@@ -30,20 +27,16 @@ public class Window {
     }
 
     public static void addComponentsToPane(Game game, Container pane) {
-        if (RIGHT_TO_LEFT)
-        {
+        if (RIGHT_TO_LEFT) {
             pane.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
         }
 
         JButton button;
-        JLabel label;
 
-        pane.setSize(900, 700);
         pane.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
 
-        if (shouldFill)
-        {
+        if (shouldFill) {
             //natural height, maximum width
             c.fill = GridBagConstraints.HORIZONTAL;
         }
@@ -52,10 +45,9 @@ public class Window {
         displayText = new JPanel();
         displayText.setBorder(BorderFactory.createTitledBorder("Display Text"));
         displayText.setBackground(Color.yellow);
-        nextTextDisplay(displayText, "<html>WELCOME TO RISK WITH BOT HARMON!<br>Enter First Player Name </html>" );
+        nextTextDisplay(displayText, "<html>WELCOME TO RISK WITH BOT HARMON!<br>Enter First Player Name </html>");
         //Setting the display constraints
-        if (shouldWeightX)
-        {
+        if (shouldWeightX) {
             c.weightx = 0.6;
         }
         c.fill = GridBagConstraints.BOTH;
@@ -76,9 +68,13 @@ public class Window {
         button.addActionListener(e -> {
             String input = textArea.getText();
             textArea.setText("");
-            nextInstruction(game.getPlayers(), input);
+            if (instruction <= 2) {
+                nextInstructionBeforeStartGame(game.getPlayers(), input);
+            } else if (instruction > 2){
+                nextInstruction(input);
+            }
+            System.out.println("Button working, current instruction :" + instruction);
         });
-
 
         c.fill = GridBagConstraints.BOTH;
         c.weightx = 1.5;
@@ -89,11 +85,9 @@ public class Window {
         readText.add(button, BorderLayout.PAGE_END);
         pane.add(readText, c);
 
-
         //DECK BUTTON
         Image card = null;
-        try
-        {
+        try {
             card = ImageIO.read(Window.class.getResource("/images/card.jpeg"));
         } catch (IOException e) {
             e.printStackTrace();
@@ -106,37 +100,30 @@ public class Window {
         c.gridy = 0;
         pane.add(button, c);
 
-        //MAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAP
+        //MAP PANEL
         JPanel mapPanel = new JPanel(new GridBagLayout());
-        
-        //label = new JLabel(new ImageIcon(Window.class.getResource("/images/map.png")));
         mapPanel.setBorder(BorderFactory.createTitledBorder("MAP"));
         c.fill = GridBagConstraints.BOTH;
         c.ipady = 70;      //make this component tall
+        c.ipadx = 200;
         c.weightx = 0.0;
         c.gridwidth = 3;
         c.gridx = 0;
         c.gridy = 0;
-        c.insets = new Insets(0, 0, 0, 0);
-       
-        //mapPanel.add(label, c);
 
+        //mapPanel.add(label, c);
         mapPanel.add(game.getMap(), c);
-        
 
         c.ipady = 0;
         c.gridy = 1;
         pane.add(mapPanel, c);
         pane.validate();
         pane.repaint();
-
     }
 
     //Method that will handle the next instruction, we maybe need to make a class for this
-    public static void nextInstruction(Player[] players, String input)
-    {
-        switch(instruction)
-        {
+    public static void nextInstructionBeforeStartGame(Player[] players, String input) {
+        switch (instruction) {
             case 1:
                 players[0].setName(input);
                 nextTextDisplay(displayText, "<html>Thanks for that! <br>Now Enter Second Player Name </html>");
@@ -144,6 +131,7 @@ public class Window {
             case 2:
                 players[1].setName(input);
                 nextTextDisplay(displayText, "<html>We are now ready to start the game!<br> See you on next Assignment <3 </html>");
+                System.out.println(players);
                 break;
             default:
                 break;
@@ -151,22 +139,33 @@ public class Window {
         instruction++;
     }
 
+    //Method that will handle the next instruction, we maybe need to make a class for this
+    public static void nextInstruction(String input) {
+        switch (instruction) {
+            case 3:
+                //next assignment
+                break;
+            case 4:
+                //next assignment
+                break;
+            default:
+                break;
+        }
+        instruction++;
+    }
 
-    private static void nextTextDisplay(JPanel jp, String message)
-    {
-        jp.removeAll(); 
+    private static void nextTextDisplay(JPanel jp, String message) {
+        jp.removeAll();
         labelDisplayText = new JLabel(message);
         jp.add(labelDisplayText);
 
         jp.updateUI();
-        System.out.println("button working");
+        System.out.println("Text Display Updated");
     }
 
 
-
-    public static void createAndShowGUI(Game game)
-    {
-    	//Create and set up the window.
+    public static void createAndShowGUI(Game game) {
+        //Create and set up the window.
         JFrame frame = new JFrame("GridBagLayoutDemo");
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
