033d5d739d478fffb7d811fc893995fb373e10df
BecaBuarque <77796842+BecaBuarque@users.noreply.github.com>
2021-02-19 13:01:34 +0000

    New Layout
    Need to fix the bug of updating the ui and minimise

--- src/ui/Window.java
+++ src/ui/Window.java
@@ -5,49 +5,44 @@ import com.botharmon.Game;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.io.IOException;
 import javax.swing.border.TitledBorder;
+import javax.swing.AbstractAction;
 
 
 /*
  * @Author: BotHarmon (Jess Dempsey, Rebeca Buarque, Noemi Banal)
  */
-public class Window
-{
-    final static boolean shouldFill = true;
+public class Window {
     final static boolean shouldWeightX = true;
     final static boolean RIGHT_TO_LEFT = false;
 
     private static DisplayText displayText;
     private static CommandPanel commandPanel;
 
-    public Window(Game risk)
-    {
+    public Window(Game risk) {
         displayText = new DisplayText();
         commandPanel = new CommandPanel(risk.getActivePlayers());
         createAndShowGUI(risk);
     }
 
     // used to print the current message for each player's turn
-    public void getTextDisplay(String message)
-    {
+    public void getTextDisplay(String message) {
         displayText.getTextDisplay(message);
     }
-    
+
     // used to print an error message in response to the players' erroneous input
-    public void sendErrorMessage(String message)
-    {
+    public void sendErrorMessage(String message) {
         displayText.sendErrorMessage(message);
     }
-    
+
     // takes the current instruction for each player
-    public String getCommand()
-    {
+    public String getCommand() {
         return commandPanel.getCommand();
     }
 
-    public static void createAndShowGUI(Game risk)
-    {
+    public static void createAndShowGUI(Game risk) {
         //Create and set up the window.
         JFrame frame = new JFrame("Risk");
         frame.setResizable(false);
@@ -62,36 +57,40 @@ public class Window
         frame.setVisible(true);
     }
 
-    public static void addComponentsToPane(Game risk, Container pane)
-    {
+    public static void addComponentsToPane(Game risk, Container pane) {
         if (RIGHT_TO_LEFT) {
             pane.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
         }
-        
-        pane.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
 
-        if (shouldFill) {
-            //natural height, maximum width
-            c.fill = GridBagConstraints.HORIZONTAL;
-        }
+        pane.setLayout(new GridBagLayout());
+        GridBagConstraints cDisplay = new GridBagConstraints();
+        GridBagConstraints cMap = new GridBagConstraints();
+        GridBagConstraints cButton = new GridBagConstraints();
+        GridBagConstraints cCommand = new GridBagConstraints();
 
         //Setting the display constraints
         if (shouldWeightX) {
-            c.weightx = 1;
+            cDisplay.weightx = 1;
+            cMap.weightx = 1;
+            cCommand.weightx = 1;
+            cButton.weightx = 1;
         }
-        c.fill = GridBagConstraints.BOTH;
-        c.gridx = 0;
-        c.gridy = 0;
 
-        pane.add(displayText, c);
+        cDisplay.fill = GridBagConstraints.BOTH;
+        cDisplay.gridx = 0;
+        cDisplay.gridy = 0;
+        cDisplay.gridheight = 7;
+        cDisplay.ipadx = 100;
 
-        c.fill = GridBagConstraints.BOTH;
-        c.weightx = 1.5;
-        c.gridx = 1; //column
-        c.gridy = 0;
 
-        pane.add(commandPanel, c);
+        pane.add(displayText, cDisplay);
+
+        cCommand.fill = GridBagConstraints.BOTH;
+        cCommand.gridx = 1;
+        cCommand.gridy = 0;
+        cCommand.ipadx = 590;
+
+        pane.add(commandPanel, cCommand);
 
         //DECK BUTTON
         Image card = null;
@@ -103,28 +102,27 @@ public class Window
 
         // change to image icon
         JButton button = new JButton(new ImageIcon(card));
-        c.fill = GridBagConstraints.BOTH;
-        c.weightx = 0;
-        c.gridx = 2;
-        c.gridy = 0;
-        pane.add(button, c);
+        cButton.fill = GridBagConstraints.BOTH;
+        cButton.gridx = 2;
+        cButton.gridy = 0;
+        cButton.ipadx = 15;
+
+        pane.add(button, cButton);
 
         TitledBorder titledBorderMap = BorderFactory.createTitledBorder("MAP");
         risk.getMap().setBorder(titledBorderMap);
         titledBorderMap.setTitleColor(Color.WHITE);
         risk.getMap().setBackground(Color.black);
-        c.fill = GridBagConstraints.BOTH;
-        c.ipady = 70;      //make this component tall
-        c.ipadx = 200;
-        c.weightx = 0.0;
-        c.gridwidth = 3;
-        c.gridx = 0;
-        c.gridy = 0;
 
-        c.ipady = 0;
-        c.gridy = 1;
-        
-        pane.add(risk.getMap(), c);
+        cMap.fill = GridBagConstraints.BOTH;
+        cMap.gridx = 1;
+        cMap.gridy = 1;
+        cMap.ipady = 150;      //make this component tall
+        cMap.ipadx = 220;
+        cMap.weightx = 0.0;
+        cMap.gridwidth = 3;
+
+        pane.add(risk.getMap(), cMap);
         pane.validate();
         pane.repaint();
     }
