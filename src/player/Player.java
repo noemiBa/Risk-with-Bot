d0a4578c7e2d7298c78b5d180a993ff76350625e
BecaBuarque <77796842+BecaBuarque@users.noreply.github.com>
2021-02-27 18:30:27 +0000

    Add files via upload

--- src/player/Player.java
+++ src/player/Player.java
@@ -9,63 +9,59 @@ import ui.Map;
 import java.awt.*;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+
 import lib.CustomArrayList;
+
 import java.util.Collections;
 
-/** Player abstract class extended by ActivePlayer and PassivePlayer
- * */
-public abstract class Player
-{
+/**
+ * Player abstract class extended by ActivePlayer and PassivePlayer
+ */
+public abstract class Player {
     private String name;
     private Color playerColor;
-    private ArrayList <Card> cards;
+    private ArrayList<Card> cards;
     private CustomArrayList<Country> countriesControlled;
 
-    public Player(String name, Color playerColor)
-    {
+    public Player(String name, Color playerColor) {
         this.name = name;
         this.playerColor = playerColor;
-        cards = new ArrayList <Card>();
+        cards = new ArrayList<Card>();
         countriesControlled = new CustomArrayList<Country>();
     }
 
-    public Player(){}
-    
+    public Player() {
+    }
+
     /*Accessor methods*/
-    public CustomArrayList <Country> getCountriesControlled()
-    {
+    public CustomArrayList<Country> getCountriesControlled() {
         return countriesControlled;
     }
 
-    public String getName()
-    {
+    public String getName() {
         return name;
     }
-    
-    public Color getPlayerColor()
-    {
+
+    public Color getPlayerColor() {
         return playerColor;
     }
-    
-    public ArrayList<Card> getCards()
-    {
+
+    public ArrayList<Card> getCards() {
         return cards;
     }
-    
+
     /*Mutator methods*/
-    public void setName(String name)
-    {
+    public void setName(String name) {
         this.name = name;
     }
 
-    /** Removes the number of cards specified from the top of the deck ArrayList and
+    /**
+     * Removes the number of cards specified from the top of the deck ArrayList and
      * adds to Player cards ArrayList
-     * */
-    public static void assignCountriesControlled(ActivePlayer[] activePlayers, PassivePlayer[] passivePlayers, Map map)
-    {
+     */
+    public static void assignCountriesControlled(ActivePlayer[] activePlayers, PassivePlayer[] passivePlayers, Map map) {
         ArrayList<Integer> countryIndexes = new ArrayList<Integer>();
-        for(int i = 0; i < 42; i++)
-        {
+        for (int i = 0; i < 42; i++) {
             countryIndexes.add(i);
         }
 
@@ -75,39 +71,42 @@ public abstract class Player
         // assigns random countries to each player using randomIndexes to reference the country ArrayList
 
         int j = 0;
-        for(ActivePlayer a: activePlayers)
-        {
-            for(int i = 0; i < GameData.INIT_COUNTRIES_PLAYER; i++, j++)
-            {
+        for (ActivePlayer a : activePlayers) {
+            for (int i = 0; i < GameData.INIT_COUNTRIES_PLAYER; i++, j++) {
                 int currentJ = countryIndexes.get(j);
                 a.getCountriesControlled().add(map.getCountries().get(currentJ).getName(), map.getCountries().get(currentJ));
                 a.getCountriesControlled().get(i).setControlledBy(a);
             }
         }
 
-        for(PassivePlayer p: passivePlayers)
-        {
-            for(int i = 0; i < GameData.INIT_COUNTRIES_NEUTRAL; i++, j++)
-            {
+        for (PassivePlayer p : passivePlayers) {
+            for (int i = 0; i < GameData.INIT_COUNTRIES_NEUTRAL; i++, j++) {
                 int currentJ = countryIndexes.get(j);
                 p.getCountriesControlled().add(map.getCountries().get(currentJ).getName(), map.getCountries().get(currentJ));
                 p.getCountriesControlled().get(i).setControlledBy(p);
             }
         }
     }
-    
+
     @Override
-	public String toString()
-	{
-		return "[name=" + name + ", playerColor=" + playerColor + "]";
-	}
-    
-	public String printCountries() {
-		String output = "[";
-		for (Country c : getCountriesControlled()) {
-			output = output + c.getName() + ", "; 
-		}
-		output = output.substring(0, output.length()-2) + "]"; 
-		return output;
-	}
+    public String toString() {
+        return "[name=" + name + ", playerColor=" + playerColor + "]";
+    }
+
+    public String printCountries() {
+        String output = "[";
+        for (Country c : getCountriesControlled()) {
+            output = output + c.getName() + ", ";
+        }
+        output = output.substring(0, output.length() - 2) + "]";
+        return output;
+    }
+
+    public static int numberOfCountriesControlledBy(Player player) {
+        int count = 0;
+        for (Country c : player.getCountriesControlled()) {
+            count++;
+        }
+        return count;
+    }
 }
